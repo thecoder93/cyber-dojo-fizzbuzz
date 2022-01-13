a9cbce151c342f77a5f626327fbc06adb9464bb6
ZLvLXL <ZLvLXL@cyber-dojo.org>
2022-01-13 14:15:38 +0000

    13 ran tests, predicted green, got amber

--- files/FizzBuzz.java
+++ files/FizzBuzz.java
@@ -8,6 +8,8 @@ public class FizzBuzz {
     public String answer(String number) {
         if(number.equals("3"))
            return "Fizz";
+        else if(number.equals("5"))
+            return "Buzz"
         return number;
     }
 }
