5a5fe389cdf2e4b2668e2c1ecb62bcf401dec1a6
ZLvLXL <ZLvLXL@cyber-dojo.org>
2022-01-14 17:56:22 +0000

    37 ran tests, no prediction, got green

--- files/FizzBuzz.java
+++ files/FizzBuzz.java
@@ -6,12 +6,15 @@ public class FizzBuzz {
     }
     
     public String answer(int number) {
+        if((number % 15 == 0))
+           return "FizzBuzz";
+        
         if(number % 3 == 0)
            return "Fizz";
+        
         if(number % 5 == 0)
             return "Buzz";
-        if((number % 15 == 0))
-            return "FizzBuzz";
+      
         return String.valueOf(number);
     }
 }
