d954945f4f257bf4d968d598d1b29340c645722b
ZLvLXL <ZLvLXL@cyber-dojo.org>
2022-01-14 17:50:05 +0000

    30 ran tests, no prediction, got red

--- files/FizzBuzz.java
+++ files/FizzBuzz.java
@@ -10,6 +10,8 @@ public class FizzBuzz {
            return "Fizz";
         else if(number % 5 == 0)
             return "Buzz";
+        else if( number % 3 == 0 && number % 5 == 0)
+            return "FizzBuzz";
         return String.valueOf(number);
     }
 }
